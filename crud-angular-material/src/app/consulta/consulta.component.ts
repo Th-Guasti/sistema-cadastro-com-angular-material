0a51121ee9434fed37dcec7cee8135df743c0c29
Th-Guasti <thaslesouza@gmail.com>
2025-03-24 14:06:34 -0300

    populando a lista de clientes utilizando o ciclo de vida do component

--- crud-angular-material/src/app/consulta/consulta.component.ts
+++ crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -6,6 +6,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table'
 import { MatButtonModule } from '@angular/material/button';
+import { ClienteService } from '../cliente.service';
+import { Cliente } from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
@@ -21,6 +23,16 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
+
 export class ConsultaComponent {
 
+  listaClientes: Cliente[] = [];
+
+  constructor(private service: ClienteService) {
+
+  }
+
+  ngOnInit(){
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
 }
