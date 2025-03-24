2ce9d5b5fda2a08553337178072167b2c7207a01
Th-Guasti <thaslesouza@gmail.com>
2025-03-24 13:03:11 -0300

    Lógica para cadastrar um cliente !!!

--- crud-angular-material/src/app/cliente.service.ts
+++ crud-angular-material/src/app/cliente.service.ts
@@ -7,9 +7,28 @@ import { Cliente } from './cadastro/cliente';
 
 export class ClienteService {
 
+  static REPO_CLIENTES = "_CLIENTES";
+
   constructor() { }
 
   salvar(cliente: Cliente) {
-    console.log(cliente);
+    const storage = this.obterStorage();
+    storage.push(cliente);
+
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
+  }
+
+  obterStorage(): Cliente[] {
+    const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+    if (repositorioClientes) {
+      const clientes: Cliente[] = JSON.parse(repositorioClientes) //recebe a string de repositorioClientes e transforma em array de clientes
+      return clientes;
+    }
+
+    const clientes: Cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+    return clientes;
   }
 }
+
+//obterStorage, faz a primeira verificação para caso já tenha um array, se não houver um array, vai ser skipada a primeira verificação e na segunda etapa ele cria o array vazio e insere o cliente ali, essa segunda etapa só será rodada uma vez por código caso necessário
