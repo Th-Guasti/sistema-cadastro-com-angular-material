95db4a51536f221548179dbd6688903dbcf64604
Th-Guasti <thaslesouza@gmail.com>
2025-03-24 10:26:17 -0300

    finalizando parte visual e populando os dados do cliente pelo form

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -3,19 +3,29 @@ import { FlexLayoutModule } from '@angular/flex-layout'
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [ 
-    FlexLayoutModule, 
-    MatCardModule, 
-    FormsModule, 
-    MatFormFieldModule, 
-    MatInputModule ],
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  cliente: Cliente = Cliente.newCliente()
 
+  salvar() {
+    console.log("Dados Cliente: ", this.cliente);
+  }
 }
